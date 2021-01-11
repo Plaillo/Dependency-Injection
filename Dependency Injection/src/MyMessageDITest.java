9f82839a3c0da0a863edbca8ef2d35bae1e9781f
Niklas <niklas.bartsch@ivv.de>
2021-01-11 13:21:06 +0100

    YEP

--- Dependency Injection/src/MyMessageDITest.java
+++ Dependency Injection/src/MyMessageDITest.java
@@ -1,22 +1,21 @@
-
 public class MyMessageDITest {
-	
+
 	public static void main(String[] args) {
 		String msg = "Hallo Welt";
 		String email = "test@abc.com";
 		String phone = "+491111111123239";
 		MessageServiceInjector injector = null;
 		Consumer app = null;
-		
+
 		// Versende Email
 		injector = new EmailServiceInjector();
 		app = injector.getConsumer();
 		app.processMessages(msg, email);
-		
+
 		// Versende SMS
 		injector = new SMSServiceInjector();
 		app = injector.getConsumer();
 		app.processMessages(msg, phone);
-		
+
 	}
 }
