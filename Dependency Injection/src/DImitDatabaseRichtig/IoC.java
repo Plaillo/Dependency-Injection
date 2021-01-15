ab5e2095797f9ca8301d0798e98cc65fa621a420
Marvin <marvin.radmer@hotmail.de>
2021-01-15 13:32:21 +0100

    Neue Klasse

--- Dependency Injection/src/DImitDatabaseRichtig/IoC.java
+++ Dependency Injection/src/DImitDatabaseRichtig/IoC.java
@@ -1,5 +1,36 @@
 package DImitDatabaseRichtig;
 
+
+
 public class IoC {
 
-}
+		public static void main(String[] args) {
+			IoC container = new IoC();
+			User user = container.new User(container.new MySqlDatabase());
+			user.add("This is some data");
+			
+		}
+		
+		
+		public class User {
+			MySqlDatabase database;
+			
+			public User(MySqlDatabase database) {
+				this.database = database;
+			}
+			
+			public void add(String data) {
+				database.persist(data);
+			}
+		}
+		
+		public class MySqlDatabase {
+			
+			public void persist(String data) {
+				System.out.println("Mysql has persisted: " + data);
+			}
+		}
+		
+		
+		
+	}
